29e3c332b2093a674e2621c44c1ec1f3af87c928
balza1979 <balza1979@live.com>
2026-04-28 16:02:24 +0200

    Add files via upload

---
+++ js/x2_loader.js
@@ -0,0 +1,19 @@
+// x2_loader.js
+// Carica il JSON corretto dalla cartella json_tendine
+
+function x2_caricaJSON(nomeFile, callback) {
+
+    fetch("json_tendine/" + nomeFile + ".json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Errore fetch JSON: " + nomeFile);
+            }
+            return response.json();
+        })
+        .then(data => {
+            callback(data);
+        })
+        .catch(err => {
+            console.error("Errore:", err);
+        });
+}
