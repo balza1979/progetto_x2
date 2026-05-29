e3e7446f444e78c3c23c7dc8937769d8174f1790
balza1979 <balza1979@live.com>
2026-05-29 13:54:26 +0200

    Update confronto_memorie.js

--- hex/confronto_memorie.js
+++ hex/confronto_memorie.js
@@ -132,6 +132,13 @@ document.addEventListener("DOMContentLoaded", function () {
     setupModalitaCreazione();
 });
 
+// 8) Nascondi pulsante "Visualizza TUTTI i parametri"
+document.querySelectorAll("button").forEach(btn => {
+    if (btn.textContent.toLowerCase().includes("tutti i parametri")) {
+        btn.style.display = "none";
+    }
+});
+
 
 
 // ------------------------------------------------------------
